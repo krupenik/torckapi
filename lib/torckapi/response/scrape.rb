531b13022f23f99b98aabbe70c4aae39eb296c8f
Dennis Krupenik <dennis@krupenik.com>
2013-04-13 03:46:15 +0300

    added http implementation, version bump

--- lib/torckapi/response/scrape.rb
+++ lib/torckapi/response/scrape.rb
@@ -1,3 +1,5 @@
+require 'bencode'
+
 module Torckapi
   module Response
 
@@ -12,13 +14,27 @@ module Torckapi
       # @return [Torckapi::Response::Scrape] response
       def self.from_udp info_hashes, data
         raise Torckapi::ArgumentError, "data does not match info_hashes" if data.length != info_hashes.count * 12
-        new Hash[info_hashes.zip(data.unpack('a12' * info_hashes.count).map { |i| peers_hash(i) })]
+        new Hash[info_hashes.zip(data.unpack('a12' * info_hashes.count).map { |i| counts_unpacked(i) })]
+      end
+
+      def self.from_http data
+        bdecoded_data = BEncode.load(data)
+        raise Torckapi::ArgumentError, "invalid data" unless bdecoded_data.is_a? Hash and bdecoded_data.has_key? 'files'
+        new Hash[bdecoded_data["files"].map { |info_hash, counts| [info_hash.unpack('H*').join, counts_translated(counts) ]}]
       end
 
       private
 
-      def self.peers_hash data
-        Hash[[:seeders, :completed, :leechers].zip(data.unpack('L>3').map(&:to_i))]
+      def self.counts_unpacked data
+        counts_with_block(data, lambda { |data| data.unpack('L>3').map(&:to_i) })
+      end
+
+      def self.counts_translated data
+        counts_with_block(data, lambda { |data| data.values_at("complete", "downloaded", "incomplete") })
+      end
+
+      def self.counts_with_block data, block
+        Hash[[:seeders, :completed, :leechers].zip(block.call(data))]
       end
 
       def initialize data
--- lib/torckapi/tracker/http.rb
+++ lib/torckapi/tracker/http.rb
@@ -9,9 +9,7 @@ module Torckapi
       # (see Base#announce)
       def announce info_hash
         super info_hash
-
-        @url.query ||= ""
-        @url.query += "info_hash=%s" % URI.encode([info_hash].pack('H*'))
+        @url.query += info_hash_params [info_hash]
 
         Torckapi::Response::Announce.from_http info_hash, Net::HTTP.get(@url)
       end
@@ -19,11 +17,23 @@ module Torckapi
       # (see Base#scrape)
       def scrape info_hashes=[]
         super info_hashes
+        @url.query += info_hash_params info_hashes
+        @url.path.gsub!(/announce/, 'scrape')
 
-        raise NotImplementedError
+        Torckapi::Response::Scrape.from_http Net::HTTP.get(@url)
       end
 
       private
+
+      def initialize url, options={}
+        super url, options
+
+        @url.query ||= ""
+      end
+
+      def info_hash_params info_hashes
+        info_hashes.map { |i| "info_hash=%s" % URI.encode([i].pack('H*')) }.join('&')
+      end
     end
   end
 end
