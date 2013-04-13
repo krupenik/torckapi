49351c16ad6662f501661c201eeffc875475f1c7
Dennis Krupenik <dennis@krupenik.com>
2013-04-13 04:02:48 +0300

    dealing with http timeouts, version bump

--- lib/torckapi/tracker/http.rb
+++ lib/torckapi/tracker/http.rb
@@ -5,30 +5,39 @@ module Torckapi
   module Tracker
 
     class HTTP < Base
+      REQUEST_ACTIONS = [Announce = 1, Scrape = 2].freeze
 
       # (see Base#announce)
       def announce info_hash
         super info_hash
-        @url.query += info_hash_params [info_hash]
-
-        Torckapi::Response::Announce.from_http info_hash, Net::HTTP.get(@url)
+        Torckapi::Response::Announce.from_http(info_hash, perform_request(url_for(@url.dup, Announce, info_hash)))
       end
 
       # (see Base#scrape)
       def scrape info_hashes=[]
         super info_hashes
-        @url.query += info_hash_params info_hashes
-        @url.path.gsub!(/announce/, 'scrape')
-
-        Torckapi::Response::Scrape.from_http Net::HTTP.get(@url)
+        Torckapi::Response::Scrape.from_http(perform_request(url_for(@url.dup, Scrape, info_hashes)))
       end
 
       private
 
       def initialize url, options={}
         super url, options
-
         @url.query ||= ""
+      end
+
+      def url_for url, action, data
+        url.query += info_hash_params [*data]
+        url.path.gsub!(/announce/, 'scrape') if Scrape == action
+        url
+      end
+
+      def perform_request url
+        begin
+          Net::HTTP.get(url)
+        rescue Errno::ECONNRESET, Errno::ETIMEDOUT, Timeout::Error
+          raise CommunicationFailedError
+        end
       end
 
       def info_hash_params info_hashes
