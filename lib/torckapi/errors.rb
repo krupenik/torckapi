89641c023393d2c4b6e781823c45dec3133a5d06
Dennis Krupenik <dennis@krupenik.com>
2014-04-11 18:18:32 +0300

    typo fix, version bump

--- lib/torckapi/errors.rb
+++ lib/torckapi/errors.rb
@@ -5,7 +5,7 @@ module Torckapi
   module Tracker
     Error = Class.new(Torckapi::Error)
     InvalidSchemeError = Class.new(Error)
-    ConnectionFailedError Class.new(Error)
+    ConnectionFailedError = Class.new(Error)
     CommunicationError = Class.new(Error)
     CommunicationFailedError = Class.new(CommunicationError)
     CommunicationTimeoutError = Class.new(CommunicationError)
